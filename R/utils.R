3c318e53af0dc058c3da1c8604716abf196dab2a
Tomasz Włodarczyk <tomwlo@gmail.com>
2022-12-30 18:58:45 +0100

    documentation of exported functions

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,22 @@
-# transform ranges to single values in the specified column
+#
 # mode 1: minimal number
 # mode 2: maximal number
 # mode 3: mean number
 # mode 4: random sampling
-transform_to_sigle_val<-function(data=long_tests_results,column="aggression_num",mode=1){
+#' Change data frame by transforming ranges to single values in the specified column
+#'
+#' @param data A data frame with source data for the analysis
+#' @param column A character indicating column name with variable to be transformed
+#' @param treatment A character indicating treatment variant to be chosen for
+#' statistical comparison
+#' @param mode A numeric indicating how the range will be mapped to single variable;
+#' 1 - minimum, 2 - maximum, 3 - mean, 4 - random.
+#' @returns A data frame with transformed variable
+#' @export
+transform_to_sigle_val <- function(data = long_tests_results, column="aggression_num", mode=1){
   fun_list=list(min,max,mean,select_from_range)
-  data[,column]<-mapply(function(x) fun_list[[mode]](get_extremes(x)),data[,column])
-  data[,column]<-as.numeric(as.character(data[,column]))
+  data[,column] <- mapply(function(x) fun_list[[mode]](get_extremes(x)),data[,column])
+  data[,column] <- as.numeric(as.character(data[,column]))
   data
 }
 
@@ -57,12 +67,26 @@ hitherto_number<-function(test_id=NA, time_limit=10, column="aggression_num",
   max(na.omit(vals))
 }
 
-# perform the chi-square test for the number of colonies in which
-# the number of aggressive F. fusca was greater then threshold value (defaults to 0);
-# the tested factor is location of F. fusca colony relative to red wood ant territory
+#' Perform the chi-square test for the number of colonies in which
+#' the number of aggressive F. fusca was greater then threshold value (defaults to 0);
+#' the tested factor is location of F. fusca colony relative to red wood ant territory
+#'
+#' @param upper_limit_date A character to be coerced to class 'Date' indicating
+#' upper bound to filter experiments by completion date
+#' @param lower_limit_date A character to be coerced to class 'Date' indicating
+#' lower bound to filter experiments by completion date
+#' @param treatment A character indicating treatment variant to be chosen for
+#' statistical comparison
+#' @param duration A numeric indicating the number of minutes from the experiment begin
+#' to be considered when determining aggression occurrence.
+#' @param threshold_number A numeric indicating the minimal number of ants being
+#' aggressive above which the test is classified as showing aggression
+#' @param data A data frame with source data for the analysis
+#' @returns Output from stats::chisq.test and medians of thr compared variables
+#' @export
 chi_sq_test <- function(upper_limit_date="2020-12-31", lower_limit_date="2017-01-01",
                       treatment_="sanguinea_1", duration=60, threshold_number=0,
-                      data, ...){
+                      data = aggression_probing_tests, ...){
   filtered_data <- filter(data,date < as.Date(upper_limit_date),
                         date > as.Date(lower_limit_date), treatment==treatment_)
   filtered_data <- transform_to_sigle_val(data=filtered_data,...)
